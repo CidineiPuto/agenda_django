3fcf73650634ce3e3bc8079fdd0d3a74ca3689fb
CidineiPuto <rafaelalvesqueiroz02@gmail.com>
2023-09-27 18:30:26 -0300

    Configuration of the ContactAdmin

--- contact/admin.py
+++ contact/admin.py
@@ -5,4 +5,19 @@ from contact.models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "phone",
+    )
+    ordering = ("-id",)
+    search_fields = (
+        "id",
+        "first_name",
+        "last_name",
+        "phone",
+    )
+    list_per_page = 10
+    list_max_show_all = 200
+    list_display_links = "phone"
