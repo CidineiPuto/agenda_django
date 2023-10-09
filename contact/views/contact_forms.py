b8b10fab2bbc8a2efb6f633734db46775753fada
CidineiPuto <rafaelalvesqueiroz02@gmail.com>
2023-10-09 17:11:38 -0300

    Validation using clean and clean_field.

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,28 +1,6 @@
-from typing import Any
+from django.shortcuts import render
 
-from django import forms
-from django.core.exceptions import ValidationError
-from django.core.paginator import Paginator  # type: ignore
-from django.db.models import Q  # type: ignore
-from django.shortcuts import get_object_or_404  # type: ignore
-from django.shortcuts import redirect, render  # type: ignore
-
-from contact.models import Contact
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            "first_name",
-            "last_name",
-            "phone",
-        )
-
-    def clean(self) -> dict[str, Any]:
-        cleaned_data = self.cleaned_data
-        self.add_error("first_name", ValidationError("Mensagem", code="invalid"))
-        return super().clean()
+from contact.forms import ContactForm
 
 
 def create(request):
