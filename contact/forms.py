fe30817fd7ed3fd1c7cb4377ae120890cd952ecb
CidineiPuto <rafaelalvesqueiroz02@gmail.com>
2023-10-13 17:22:56 -0300

    User update form

--- contact/forms.py
+++ contact/forms.py
@@ -123,6 +123,42 @@ class RegisterUpdateForm(forms.ModelForm):
         required=False,
     )
 
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "email",
+            "username",
+        )
+
+    def save(self, commit=True):
+        clean_data = self.cleaned_data
+        user = super().save(commit=False)
+
+        password = clean_data.get("password1")
+
+        if password:
+            user.set_password(password)
+
+        if commit:
+            user.save()
+
+        return user
+
+    def clean(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+
+        if password1 or password2:
+            if password1 != password2:
+                self.add_error(
+                    "password2",
+                    ValidationError("Senhas não batem"),
+                )
+
+        return super().clean()
+
     def clean_email(self):
         email = self.cleaned_data.get("email")
         current_email = self.instance.email
@@ -137,11 +173,16 @@ class RegisterUpdateForm(forms.ModelForm):
                     ),
                 )
 
-    class Meta:
-        model = User
-        fields = (
-            "first_name",
-            "last_name",
-            "email",
-            "username",
-        )
+    def clean_password1(self):
+        password1 = self.cleaned_data.get("password1")
+
+        if password1:
+            try:
+                password_validation.validate_password(password1)
+            except ValidationError as errors:
+                self.add_error(
+                    "password1",
+                    ValidationError(errors),
+                )
+
+        return password1
